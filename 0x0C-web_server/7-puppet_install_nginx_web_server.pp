fcfad4803f9f89503bbde6c153cb7ce525d6cf8f
GracyDinma <okohagchidinma@gmail.com>
2024-07-13 15:06:35 -0700

    puppet install

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,16 +4,18 @@ package { 'nginx':
   ensure => installed,
 }
 
-file_line {'install':
+file_line { 'install':
   ensure => 'present',
   path   => '/etc/nginx/sites-enabled/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.github.com/GracyDinma permanent;',
+}
+
 file { '/var/www/html/index.html':
-  content => 'Hello World'.
+  content => 'Hello World!',
 }
 
 service { 'nginx':
-  ensure  => running.
-  require => Package['nginx'].
+  ensure  => running,
+  require => Package['nginx'],
 }
