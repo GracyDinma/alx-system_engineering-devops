d4fd4224d20cd8761ef5d5ea1654dabe70420f30
GracyDinma <okohagchidinma@gmail.com>
2024-08-13 18:53:21 -0700

    fixed documnetation

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,33 +5,15 @@ A function that queries the Reddit API and returns number of subscribers
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of sbscribers (not active
-    users, total subscribers) for a given subreddit.
-
-    Args:
-        Subreddit (str): The ane of the subreddit to query.
-
-    Returns:
-        int: Number of subscribers if the subreddit exists, else 0.
-    """
-
-    if not isinstance(subreddit, str) or not subreddit:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {
-        'User-Agent': 'Google Chrome version 127.0.6533.99'
-        }
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        if response.status_code == 200:
-            results = response.json()
-            retrn results.get('data', {}).get('subscribers', 0)
-        else:
-            return 0
-        except requests.RequestException:
-            return 0
